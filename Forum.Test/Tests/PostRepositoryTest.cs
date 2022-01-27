db215375784c781ed1be3a4f974aa9bf5d8e3697
Michał Wójcik <01153102@pw.edu.pl>
2022-01-27 16:04:26 +0100

    Final

--- Forum.Test/Tests/PostRepositoryTest.cs
+++ Forum.Test/Tests/PostRepositoryTest.cs
@@ -29,73 +29,55 @@ namespace Forum.Test.Tests
         [TestMethod]
         public async Task GetAsync_ShouldReturnPost_WhenPostExists()
         {
-            //Setup DbContext and DbSet mock  
-            _dbSetMock.Setup(s => s.FindAsync(It.IsAny<Guid>())).Returns(new ValueTask<Post>(Task.FromResult(new Post())));
+            _dbSetMock.Setup(s => s.FindAsync(It.IsAny<int>())).Returns(new ValueTask<Post>(Task.FromResult(new Post())));
             _dbContextMock.Setup(s => s.Set<Post>()).Returns(_dbSetMock.Object);
 
-            //Execute method of SUT (ProductsRepository)  
             var post = await Task.FromResult(_postRepository.GetAsync(1));
 
-            //Assert  
             Assert.IsNotNull(post);
         }
 
         [TestMethod]
         public async Task UpdateAsync_ShouldUpdatePost_WhenPostExists()
         {
-            //Setup DbContext and DbSet mock  
             _dbSetMock.Setup(s => s.Update(It.IsAny<Post>())).Verifiable();
             _dbContextMock.Setup(s => s.Set<Post>()).Returns(_dbSetMock.Object);
 
-            //Execute method of SUT (ProductsRepository)  
             var result = await Task.FromResult(_postRepository.UpdateAsync(new Post()));
 
-            //Assert  
             Assert.IsNotNull(result);
         }
 
         [TestMethod]
         public async Task AddAsync_ShouldAddPost()
         {
-            //Setup DbContext and DbSet mock  
-
             _dbSetMock.Setup(s => s.Add(It.IsAny<Post>())).Verifiable();
             _dbContextMock.Setup(s => s.Set<Post>()).Returns(_dbSetMock.Object);
 
-            //Execute method of SUT (ProductsRepository)  
             var result = await Task.FromResult(_postRepository.AddAsync(new Post()));
-
-            //Assert  
+ 
             Assert.IsNotNull(result);
         }
 
         [TestMethod]
         public async Task DeleteAsync_ShouldDeletePost_WhenPostExists()
         {
-            //Setup DbContext and DbSet mock  
-
             _dbSetMock.Setup(s => s.Remove(It.IsAny<Post>())).Verifiable();
             _dbContextMock.Setup(s => s.Set<Post>()).Returns(_dbSetMock.Object);
 
-            //Execute method of SUT (ProductsRepository)  
             var result = await Task.FromResult(_postRepository.DelAsync(new Post()));
 
-            //Assert  
             Assert.IsNotNull(result);
         }
 
         [TestMethod]
         public async Task FindAllAsync_ShouldReturnPostList()
-        {
-            //Setup DbContext and DbSet mock  
-
+        { 
             _dbSetMock.Setup(s => s.AsAsyncEnumerable()).Verifiable();
             _dbContextMock.Setup(s => s.Set<Post>()).Returns(_dbSetMock.Object).Verifiable();
 
-            //Execute method of SUT (ProductsRepository)  
             var postsList = await Task.FromResult(_postRepository.BrowseAllAsync());
 
-            //Assert  
             Assert.IsNotNull(postsList);
         }
     }
